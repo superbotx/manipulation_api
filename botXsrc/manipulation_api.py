479dbbeb65e9b0cf675a8194465e2bfd17afc282
ashis <ashisghosh@live.com>
2018-04-20 15:55:04 -0700

    inits rosservice

--- botXsrc/manipulation_api.py
+++ botXsrc/manipulation_api.py
@@ -12,21 +12,35 @@ from sensor_msgs.msg import Image, PointCloud2, CameraInfo
 import matplotlib.pyplot as plt
 import matplotlib.pyplot as pyplot
 
-import sys
-sys.path.append('external_modules')
+from moveit_msgs.msg import MotionPlanRequest
+from moveit_msgs.srv import GetMotionPlan
+
+from haptica_manipulation.srv import PathFromPose
+
 
 import time
 import os
 
-from .path_planner import PathPlanner
 
 class ManipulationAPI(BaseComponent):
 
     def setup(self):
-        path_playnner = PathPlanner()
+        command = "roslaunch haptica_manipulation path_planner.launch"
+        self.proc_id = external_command_pool.start_command(command)
+
+        rospy.wait_for_service("/plan_path")
+        self.plan = rospy.ServiceProxy("/plan_path", PathFromPose)
+
         pass
   
 
+    def plan_path(self, pose):
+        path = self.plan(pose)
+        print(path)
+        return path
+        # pass
+
     def shutdown(self):
+        external_command_pool.end_command(self.proc_id)
         pass
 
